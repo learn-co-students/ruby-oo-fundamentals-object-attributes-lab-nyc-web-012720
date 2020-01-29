a90e36674d7c8138aa99e007969beaedae42111c
calm-fork-7535 <komald.4@gmail.com>
2020-01-29 18:43:10 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,41 @@
+
 class Person
-  def name=(name)
-    @name=name
-  end
+
   def name
     @name
   end
-  def job=(job)
-    @job=job
+
+  def name=(name)
+    @name = name
   end
-  def job
-    @job
+
+  def jobs
+    @jobs
+  end
+
+  def jobs
+    =(jobs
+    )
+    @jobs
+     = jobs
+     
   end
 end
+
+
+
+# class Person
+#   def name=(name)
+#     @name=name
+#   end
+#   def name
+#     @name
+#   end
+#   def jobs
+=(job)
+#     @job=job
+#   end
+#   def job
+#     @job
+#   end
+# end
